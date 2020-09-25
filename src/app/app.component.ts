f5b7223e2eb7bba6f93ff99749d119087996fa9d
Leonardo Gil <leonardo.lgc@policiacientifica.sp.gov.br>
2020-09-24 21:42:08 -0300

    Terminado PDF 02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'usjt-ccp1bua-data-binding';
+  // nome: string = "Jose"
+  // idade = 25
+
+  // lancarDado(): number {
+  //   return Math.floor(Math.random() * 6) + 1
+  // }
+
+  nome: string
+  exibeCaixa: boolean = false
+
+  alterarNome(nome): void {
+    console.log(nome.target.value)
+    this.nome = nome.target.value
+  }
+  adicionar(nomeInput): void {
+    console.log(nomeInput.value)
+    this.nome = nomeInput.value
+    this.exibeCaixa = true
+  }
 }
